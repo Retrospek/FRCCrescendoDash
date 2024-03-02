50228ccbf5dec52766fc9f461cb1c580c5723898
Retrospek <134454781+Retrospek@users.noreply.github.com>
2024-03-02 16:40:39 -0600

    Update Home.py

--- Home.py
+++ Home.py
@@ -1,20 +1,44 @@
 import streamlit as st
 import Calc_Web as cw
 
+def main():
+    
+    # Google Authentication
+    
 
-st.set_page_config(
-    page_title="Hello",
-    page_icon="👋",
-    layout='wide'
-)
+    st.set_page_config(
+        page_title="Hello",
+        page_icon="👋",
+        layout='wide'
+    )
 
-st.title("Scouting Application")
+    st.title("Scouting Application")
 
-st.sidebar.success("Select a Function Above")
+    if 'data' not in st.session_state:
+        st.session_state['data'] = st.file_uploader("Tournament Data")
 
-st.markdown("""This app's main function is to provide statistics that i've assembeled from FRC Crescendo 2024 data. 
-            Additionally, I've done feature engineering to create more insightful data for any user coming here""")
 
-st.markdown("""I aimed to create an app that could predict the likelihood of any given three teams defeating any other three teams. This required me to research how to compare two comparable averages and standard deviations. I considered using my experience with neural networks for this predictive task, thinking that some level of machine learning or even deep learning could be beneficial. However, I faced a data shortage. I realized that to create a predictive model that considers at least four attributes, I would need approximately 40 data inputs, based on the 10x rule. Given this, I pivoted towards a more statistics-based approach. Using the normal cumulative distribution function required normalized data. I examined past matches and found that as a team advances through a tournament, it will have a normally distributed graph of data points representing points scored. With this insight, I generated a cumulative average and standard deviation for each alliance and applied the norm.cdf function to calculate the possibility of the Blue team winning.
+    st.sidebar.success("Select a Function Above")
 
-My match prediction algorithm for the FRC District Event at Waco accurately predicted the outcomes of 88% of the first 54 matches. This accuracy rate is 14% higher than that of Statbotics, which predicted matches with 74% accuracy.""")
+    st.markdown("""This app's main function is to provide statistics that i've assembeled from FRC Crescendo 2024 data. 
+                Additionally, I've done feature engineering to create more insightful data for any user coming here""")
+
+    st.markdown("# Set-Up")
+    st.write("""I wanted to create a scouting prediction app that would take the likelyhood of
+            any given 3 teams beating any other 3 teams. To accomplish this, I researched how to 
+            compare two comparable averages and standard deviations. Additionally, 
+            I had experience in neural networks and because this was 
+            a predictive task, I thought that perhaps some level of ML or even deep
+            learning could be used. However, I ran into the issue of a lack of data, and I quickly
+            realized that in order for me to create a predictive model that took at least 4 attributes 
+            that it would need approximately 40 data inputs, according to the 10x rule
+            . With this knowledge, I instead went towards a more statistics based approach.
+            The method of using a normal cumumalitive distribution function required normalized data
+            . Knowing this I delved into past matches, and proved that as a team progresses throughout
+            a tournament it will have a normally distributed graph of data points that represent points scored.
+            Knowing this I generated a cumulative average and standard deviation for each of the alliances and 
+            applied the norm.cdf function to calculate the possibility of the Blue team winning. """)
+    
+
+if __name__ == "__main__":
+    main()
