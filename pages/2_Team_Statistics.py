84eddef8aa45e5923e2c13b76ba225d06fb92286
Retrospek <134454781+Retrospek@users.noreply.github.com>
2024-03-03 11:17:33 -0600

    Update 2_Team_Statistics.py

--- pages/2_Team_Statistics.py
+++ pages/2_Team_Statistics.py
@@ -19,15 +19,8 @@ try:
     Blue2 = int(Blue_Teams[1]) 
     Blue3 = int(Blue_Teams[2]) 
     Blue_Pred, Red_Pred = cw.match_prediction(team_stats=team_stats, Red1=Red1, Red2=Red2, Red3=Red3, Blue1=Blue1, Blue2=Blue2, Blue3=Blue3)
-    col1, col2 = st.columns(2)
-    col1.st.write("# Red Win Prediction: ", Red_Pred)
-    col1.st.button(f'{Red1}')
-    col1.st.button(f'{Red2}')
-    col1.st.button(f'{Red3}')
-    col2.st.write("# Blue Win Prediction: ", Blue_Pred)
-    col2.st.button(f'{Blue1}')
-    col2.st.button(f'{Blue2}')
-    col2.st.button(f'{Blue3}')
+    st.write("# Red Win Prediction: ", Red_Pred)
+    st.write("# Blue Win Prediction: ", Blue_Pred)
 except:
     st.markdown("# Please enter Red and Blue Alliance team numbers")
 
