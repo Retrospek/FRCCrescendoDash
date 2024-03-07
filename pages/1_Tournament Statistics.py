eb154be687e3172b867cf81ff8a7a6b24e18f9fb
Retrospek <134454781+Retrospek@users.noreply.github.com>
2024-03-07 15:42:14 -0600

    Update 1_Tournament Statistics.py

--- pages/1_Tournament Statistics.py
+++ pages/1_Tournament Statistics.py
@@ -1,29 +1,71 @@
 import streamlit as st
 import Calc_Web as cw
 import pandas as pd
+import Home as hm
+test = "WACO_2024.csv"
 
-test = "app.csv"
-any = "2024-Crescendo-FRC-2468\\data.csv"
+st.set_page_config(page_title="Tournament Statistics and Predictions",
+                   page_icon="tropy",
+                   layout='wide')
 
-st.set_page_config(page_title="Tournament Statistics and Predictions", page_icon="tropy")
+st.title("Tournament Statistics")
+
+events = ['WACO_2024.csv', 'Custom']
+
+if 'data' not in st.session_state:
+    st.session_state.data = 'WACO_2024.csv'  # Set default value
+
+# Check if the current data exists in the events list, reset if not
+if st.session_state.data not in events:
+    st.session_state.data = 'WACO_2024.csv'
+
+Data_Choice = st.selectbox('DataSet:', events, index = events.index(st.session_state.data))
+
+if Data_Choice == 'Custom':
+    Data_Provided = st.file_uploader("Your DataSet")
+    try:
+        st.session_state.data = Data_Provided
+        data = cw.get_clean_data(pd.read_csv(st.session_state.data, on_bad_lines='skip'))
+        team_stats = cw.team_desc(Data=data)
+    except:
+        st.error("Please Enter Your Data")
+
+else:
+    st.write(Data_Choice)
+    st.session_state.data = Data_Choice
+    try:
+        data = cw.get_clean_data(pd.read_csv(st.session_state.data, on_bad_lines='skip'))
+        team_stats = cw.team_desc(Data=data)
+    except:
+        st.error("Not Custom")
 
 
 
-data = cw.get_clean_data(csv= test)
-team_stats = cw.team_desc(Data=data)
-bubble = cw.plot(team_stats=team_stats)
-#cw.auto_paths(data)
+st.sidebar.header("Team Select")
+#test = "WACO_2024.csv"
+try:
 
-st.markdown("# Tournament Statistics/Predictions")
+    data = cw.get_clean_data(pd.read_csv(st.session_state.data, on_bad_lines='skip'))
 
-st.write('''This page is dedicated to analyzing teams through match predictions satatistic based calculations, 
-         and other methods of choosing the right alliance partner''')
+    team_stats = cw.team_desc(Data=data) #Get team_stats from clean data
 
-st.plotly_chart(bubble)
-
-st.write('''Using this bubble plot we can see that teams that tend to have more wins for total games have a higher average amp scored ''')
+    bubble = cw.plot(team_stats=team_stats) #plot
 
 
 
+    st.write('''This page is dedicated to analyzing teams through match predictions satatistic based calculations, 
+            and other methods of choosing the right alliance partner''')
+
+    st.plotly_chart(bubble)
+
+    st.write('''Using this bubble plot we can see that teams that tend to have more wins for total games have a higher average amp scored ''')
+
+    teams = st.multiselect("Specific Team Stats", options=team_stats['Team Number'].unique())
+
+    teamstat = team_stats[team_stats['Team Number'].isin(teams)]
+
+    st.dataframe(teamstat)
+except:
+    pass
 
 
