63938d819a047089211fddf673409b3860594cca
Retrospek <134454781+Retrospek@users.noreply.github.com>
2024-03-14 01:58:49 -0500

    Update 1_Tournament Statistics.py

--- pages/1_Tournament Statistics.py
+++ pages/1_Tournament Statistics.py
@@ -59,6 +59,8 @@ try:
 
     teamstat = team_stats[team_stats['Team Number'].isin(teams)]
 
+    teamstat['Team Number'] = teamstat['Team Number'].astype(int)
+  
     st.dataframe(teamstat.style.highlight_max(axis=0))
 except:
   pass
