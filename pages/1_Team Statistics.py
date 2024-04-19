9a540db9a852839203a08ec7d29ed67d32746b41
Retrospek <134454781+Retrospek@users.noreply.github.com>
2024-04-19 15:28:33 -0500

    Update 1_Team Statistics.py

--- pages/1_Team Statistics.py
+++ pages/1_Team Statistics.py
@@ -10,7 +10,7 @@ st.set_page_config(page_title="Tournament Statistics and Predictions",
 
 st.title("Tournament Statistics")
 
-events = ['WACO_2024.csv','Fort Worth (1).csv', 'State.csv', 'Custom']
+events = ['WACO_2024.csv','Fort Worth (1).csv', 'State.csv', 'CMP.csv', 'Custom']
 
 if 'data' not in st.session_state:
     st.session_state.data = 'WACO_2024.csv'  # Set default value
@@ -82,9 +82,28 @@ try:
 
     st.dataframe(teamstat.style.highlight_max(axis=0))
 
-    with st.expander("# :red[Similar Team List]"):
-        most_sim_robo = st.selectbox("Team Similar to the one Provided: ",options=team_stats['Team Number'].unique())
-        cw.mst_sim_rbt(most_sim_robo, team_stats)
+    #with st.expander("# :red[Similar Team List]"):
+    most_sim_robo = st.selectbox("Team Similar to the one Provided: ",options=team_stats['Team Number'].unique())
+    robot_categ = st.selectbox("Robot Category", ["Aggressive", "Consistent", "Feeder"])
+
+    weights = []
+
+    # Aggressive weights
+    aggressive_weights = [0, 0.05, 0.05, 0.1, 0.2, 0.05, 0.1, 0.1, 0.2, 0.1, 0.15, 0.05, 0.05]
+
+    # Consistent weights
+    consistent_weights = [0, 0.02, 0.02, 0.05, 0.1, 0.02, 0.05, 0.05, 0.1, 0.1, 0.1, 0.05, 0.05]
+
+    # Feeder weights
+    feeder_weights = [0, 0.01, 0.02, 0.05, 0.1, 0.01, 0.03, 0.03, 0.05, 0.05, 0.05, 0.3, 0.3] 
+    if robot_categ == 'Aggressive':
+        weights = aggressive_weights
+    elif robot_categ == 'Consistent':
+        weights = consistent_weights
+    elif robot_categ == 'Feeder':
+        weights = feeder_weights
+
+    cw.mst_sim_rbt(team=most_sim_robo, team_stats=team_stats, weights=weights)
         
     
         
@@ -96,5 +115,7 @@ try:
     ############
 
     
-except:
+
+except Exception as error:
+  st.write(error)
   pass
